06f1d847b43d4ec2493e01929b370ad2c6ea4fc1
Joker-V2 <nipinna0@gmail.com>
2022-06-16 15:50:08 +0530

    phoenix: Uprev `android.hardware.bluetooth`

--- device.mk
+++ device.mk
@@ -114,7 +114,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
